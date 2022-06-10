8c5275d7a58e27dbc75d60e45d730312e7913c00
Jabrecia Washington <jlw4958@g.rit.edu>
2022-06-10 00:38:56 -0400

    fixed and finished 1.1

--- part1/courseinfo/courseinfo/src/App.js
+++ part1/courseinfo/courseinfo/src/App.js
@@ -1,50 +1,62 @@
 const Header = (props) =>
 {
+  return(
   <div>
     <h1>{props.course}</h1>
   </div>
+  )
 }
 
 //content parts
 const Part1 = (props) =>
 {
-  <div>
-    <p>
+  return(
+    <div>
+      <p>
         {props.part1} {props.exercises1}
-    </p>
-  </div>
+      </p>
+    </div>
+  )
 }
 const Part2 = (props) =>
 {
+  return(
   <div>
     <p>
         {props.part2} {props.exercises2}
     </p>
   </div>
+  )
 }
 const Part3 = (props) =>
 {
+  return(
   <div>
     <p>
         {props.part3} {props.exercises3}
     </p>
   </div>
+  )
 }
 
 const Content = (props) =>
 {
+  return(
   <div>
     <Part1 part1={props.part1} exercises1={props.exercises1}/>
     <Part2 part2={props.part2} exercises2={props.exercises2}/>
     <Part3 part3={props.part3} exercises3={props.exercises3}/>
   </div>
+  )
 }
 
 const Total = (props) =>
 {
+  return(
   <div>
     <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
   </div>
+  )
 }
 
 const App = () => {
